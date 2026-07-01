c85fbf45ce09f95a70ab364c489af020ab74324f
sgmath12 <sgmath12@gmail.com>
2026-07-01 16:39:08 +0900

    두 번째 기능

--- checker.py
+++ checker.py
@@ -1,14 +1,15 @@
-class StringChecker:
+class AlphabetChecker:
     def __init__(self, string_a, string_b):
         self.string_a = string_a
         self.string_b = string_b
 
-    def length_score(self):
-        long_len = max(len(self.string_a), len(self.string_b))
-        short_len = min(len(self.string_a), len(self.string_b))
+    def alphabet_score(self):
+        set_a = set(c for c in self.string_a if c.isupper())
+        set_b = set(c for c in self.string_b if c.isupper())
 
-        if short_len == 0 or long_len >= 2 * short_len:
+        total = set_a | set_b
+        if not total:
             return 0
 
-        gap = long_len - short_len
-        return (1 - gap / short_len) * 60
+        same = set_a & set_b
+        return len(same) / len(total) * 40
--- test_checker.py
+++ test_checker.py
@@ -1,38 +1,38 @@
 import pytest
-from checker import StringChecker
+from checker import AlphabetChecker
 
 
-def test_same_length_gets_max_score():
-    checker = StringChecker("ASD", "DSA")
-    assert checker.length_score() == 60
+def test_same_alphabet_set_gets_max_score():
+    checker = AlphabetChecker("ASD", "DSA")
+    assert checker.alphabet_score() == 40
 
 
-def test_double_length_gets_zero():
-    checker = StringChecker("A", "BB")
-    assert checker.length_score() == 0
+def test_completely_different_alphabets_gets_zero():
+    checker = AlphabetChecker("A", "BB")
+    assert checker.alphabet_score() == 0
 
 
-def test_more_than_double_length_gets_zero():
-    checker = StringChecker("A", "BBBBB")
-    assert checker.length_score() == 0
-
-
-def test_partial_score_aaabb_baa():
-    checker = StringChecker("AAABB", "BAA")
-    assert checker.length_score() == pytest.approx(20)
+def test_same_alphabet_set_gets_max_score_with_repeats():
+    checker = AlphabetChecker("AAABB", "BAA")
+    assert checker.alphabet_score() == 40
 
 
 def test_partial_score_aa_aae():
-    checker = StringChecker("AA", "AAE")
-    assert checker.length_score() == pytest.approx(30)
+    checker = AlphabetChecker("AA", "AAE")
+    assert checker.alphabet_score() == pytest.approx(20)
 
 
 def test_order_does_not_matter():
-    checker_long_first = StringChecker("AAABB", "BAA")
-    checker_short_first = StringChecker("BAA", "AAABB")
-    assert checker_long_first.length_score() == checker_short_first.length_score()
+    checker_first = AlphabetChecker("AA", "AAE")
+    checker_second = AlphabetChecker("AAE", "AA")
+    assert checker_first.alphabet_score() == checker_second.alphabet_score()
 
 
-def test_empty_string_gets_zero():
-    checker = StringChecker("", "ABC")
-    assert checker.length_score() == 0
+def test_lowercase_letters_are_ignored():
+    checker = AlphabetChecker("aaa", "bbb")
+    assert checker.alphabet_score() == 0
+
+
+def test_empty_strings_get_zero():
+    checker = AlphabetChecker("", "")
+    assert checker.alphabet_score() == 0
