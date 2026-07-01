714bfdda5d568039e91f32f832f72c6b975b6fb6
sgmath12 <sgmath12@gmail.com>
2026-07-01 17:06:38 +0900

    Split text into two strings in constructor, move MAX_SCORE into method

--- checker.py
+++ checker.py
@@ -1,9 +1,10 @@
 class SimilarityChecker:
-    def __init__(self, string_a, string_b):
-        self.string_a = string_a
-        self.string_b = string_b
+    def __init__(self, text: str):
+        self.string_a, self.string_b = self._split_text(text)
 
-    def alphabet_score(self):
+    def get_alphabet_score(self) -> float:
+        MAX_SCORE = 40
+
         set_a = set(c for c in self.string_a if c.isupper())
         set_b = set(c for c in self.string_b if c.isupper())
 
@@ -12,4 +13,7 @@ class SimilarityChecker:
             return 0
 
         same = set_a & set_b
-        return len(same) / len(total) * 40
+        return len(same) / len(total) * MAX_SCORE
+
+    def _split_text(self, text: str) -> tuple[str, str]:
+        return text[:text.find(",")].strip(), text[text.find(",") + 1:].strip()
