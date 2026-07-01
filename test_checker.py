d7c97e324aacc88d04e770bb06c0feb74a7db6ef
sgmath12 <sgmath12@gmail.com>
2026-07-01 16:48:03 +0900

    Rename AlphabetChecker to SimilarityChecker for shared class across branches

--- test_checker.py
+++ test_checker.py
@@ -1,38 +1,38 @@
 import pytest
-from checker import AlphabetChecker
+from checker import SimilarityChecker
 
 
 def test_same_alphabet_set_gets_max_score():
-    checker = AlphabetChecker("ASD", "DSA")
+    checker = SimilarityChecker("ASD", "DSA")
     assert checker.alphabet_score() == 40
 
 
 def test_completely_different_alphabets_gets_zero():
-    checker = AlphabetChecker("A", "BB")
+    checker = SimilarityChecker("A", "BB")
     assert checker.alphabet_score() == 0
 
 
 def test_same_alphabet_set_gets_max_score_with_repeats():
-    checker = AlphabetChecker("AAABB", "BAA")
+    checker = SimilarityChecker("AAABB", "BAA")
     assert checker.alphabet_score() == 40
 
 
 def test_partial_score_aa_aae():
-    checker = AlphabetChecker("AA", "AAE")
+    checker = SimilarityChecker("AA", "AAE")
     assert checker.alphabet_score() == pytest.approx(20)
 
 
 def test_order_does_not_matter():
-    checker_first = AlphabetChecker("AA", "AAE")
-    checker_second = AlphabetChecker("AAE", "AA")
+    checker_first = SimilarityChecker("AA", "AAE")
+    checker_second = SimilarityChecker("AAE", "AA")
     assert checker_first.alphabet_score() == checker_second.alphabet_score()
 
 
 def test_lowercase_letters_are_ignored():
-    checker = AlphabetChecker("aaa", "bbb")
+    checker = SimilarityChecker("aaa", "bbb")
     assert checker.alphabet_score() == 0
 
 
 def test_empty_strings_get_zero():
-    checker = AlphabetChecker("", "")
+    checker = SimilarityChecker("", "")
     assert checker.alphabet_score() == 0
